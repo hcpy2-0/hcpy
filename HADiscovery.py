68158d5f2bec0471dc19901524fff52e5f466199
Meatballs1 <Meatballs1@users.noreply.github.com>
2025-01-30 23:00:30 +0000

    Refactor HADiscovery for full names and removal from devices.json

--- HADiscovery.py
+++ HADiscovery.py
@@ -11,141 +11,16 @@ def decamelcase(str):
 
 HA_DISCOVERY_PREFIX = "homeassistant"
 
-# These "magic overrides" provide HA MQTT autodiscovery data that is injected
-# into devices.json when `hc-login.py` is run; the data is then read from
-# devices.json at runtime.
-#
-# Note: keys should be integer (not string) taken from a device's feature
-# mapping.
+# These "magic overrides" provide HA MQTT autodiscovery data.
 MAGIC_OVERRIDES = {
-    3: {  # BSH.Common.Setting.AllowBackendConnection
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    5: {  # BSH.Common.Status.BackendConnected
-        "component_type": "binary_sensor",
-        "payload_values": {
-            "device_class": "connectivity",
-            "payload_on": True,
-            "payload_off": False,
-        },
-    },
-    21: {  # BSH.Common.Event.SoftwareUpdateAvailable
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "update", "payload_on": "Present"},
-    },
-    517: {  # BSH.Common.Status.RemoteControlStartAllowed
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    523: {  # BSH.Common.Status.RemoteControlActive
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    524: {  # BSH.Common.Setting.ChildLock
-        "component_type": "binary_sensor",
-        "payload_values": {
-            "device_class": "lock",
-            "payload_on": False,  # Lock "on" means "unlocked" to HA
-            "payload_off": True,  # Lock "off" means "locked"
-        },
-    },
-    525: {  # BSH.Common.Event.AquaStopOccured
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    527: {  # BSH.Common.Status.DoorState
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "door", "payload_on": "Open", "payload_off": "Closed"},
-    },
-    539: {  # BSH.Common.Setting.PowerState
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "power"},
-    },
-    542: {"payload_values": {"unit_of_measurement": "%"}},  # BSH.Common.Option.ProgramProgress
-    543: {  # BSH.Common.Event.LowWaterPressure
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    544: {  # BSH.Common.Option.RemainingProgramTime
+    'BSH.Common.Option.ProgramProgress': {
+        "payload_values": {"unit_of_measurement": "%"}
+    }, 
+    'BSH.Common.Option.RemainingProgramTime': { 
         "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
     },
-    549: {  # BSH.Common.Option.RemainingProgramTimeIsEstimated
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    558: {  # BSH.Common.Option.StartInRelative
+    'BSH.Common.Option.StartInRelative': {
         "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
-    },
-    4101: {  # Dishcare.Dishwasher.Status.SilenceOnDemandRemainingTime
-        "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
-    },
-    4103: {  # Dishcare.Dishwasher.Status.EcoDryActive
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    4382: {  # Dishcare.Dishwasher.Status.SilenceOnDemandDefaultTime
-        "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
-    },
-    4384: {  # Dishcare.Dishwasher.Setting.SpeedOnDemand
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    4608: {  # Dishcare.Dishwasher.Event.InternalError
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4609: {  # Dishcare.Dishwasher.Event.CheckFilterSystem
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4610: {  # Dishcare.Dishwasher.Event.DrainingNotPossible
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4611: {  # Dishcare.Dishwasher.Event.DrainPumpBlocked
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4612: {  # Dishcare.Dishwasher.Event.WaterheaterCalcified
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4613: {  # Dishcare.Dishwasher.Event.LowVoltage
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4624: {  # Dishcare.Dishwasher.Event.SaltLack
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4625: {  # Dishcare.Dishwasher.Event.RinseAidLack
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4626: {  # Dishcare.Dishwasher.Event.SaltNearlyEmpty
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    4627: {  # Dishcare.Dishwasher.Event.RinseAidNearlyEmpty
-        "component_type": "binary_sensor",
-        "payload_values": {"device_class": "problem", "payload_on": "Present"},
-    },
-    5121: {  # Dishcare.Dishwasher.Option.ExtraDry
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    5124: {  # Dishcare.Dishwasher.Option.HalfLoad
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    5127: {  # Dishcare.Dishwasher.Option.VarioSpeedPlus
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
-    },
-    5136: {  # Dishcare.Dishwasher.Option.SilenceOnDemand
-        "component_type": "binary_sensor",
-        "payload_values": {"payload_on": True, "payload_off": False},
     },
 }
 
@@ -165,6 +40,8 @@ def publish_ha_discovery(device, client, mqtt_topic):
     device_ident = device["name"]
     device_description = device.get("description", {})
 
+    base_topic = mqtt_topic.split("/")[0]
+
     version_parts = filter(
         lambda d: d is not None,
         [device_description.get("version"), device_description.get("revision")],
@@ -180,63 +57,74 @@ def publish_ha_discovery(device, client, mqtt_topic):
 
     for feature in device["features"].values():
         if "name" not in feature:
-            continue
+            continue # TODO we could display things based on UID
+
         name_parts = feature["name"].split(".")
         name = feature["name"]
         feature_id = name.lower().replace(".", "_")
-        feature_type = name_parts[-2]
-        access = feature.get("access", "none")
+        refCID = feature.get("refCID", None)
+        refDID = feature.get("refDID", None)
+        handling = feature.get("handling", None)
+        access = feature.get("access", "").lower()
         available = feature.get("available", False)
+        initValue = feature.get("initValue", None)
+        values = feature.get("values", None)
+        event_types = None
+        extra_payload_values = {}
+        value_template = "{% if '" + name + "' in value_json %}\n{{ value_json['" + name + "']|default }}\n{% endif %}"
 
-        if (
-            (
-                feature_type == "Setting"
-                and available
-                and (access == "read" or access == "readWrite")
-            )
-            or (
-                feature_type == "Status"
-                and available
-                and (access == "read" or access == "readWrite")
-            )
-            or feature_type == "Event"
-            or feature_type == "Option"
-        ):
+        if handling or refCID is None or (available and (access == "read" or access == "readwrite")): 
+            if refCID == "01" and refDID == "00":
+                component_type = "binary_sensor"
+                extra_payload_values["payload_on"] = True
+                extra_payload_values["payload_on"] = False
+            elif handling is not None:
+                component_type = "event"
+                extra_payload_values["event_types"] = list(values.values())
+                extra_payload_values["platform"] = "event"
+                value_template = "{ {% if '" + name + "' in value_json %}\n\"event_type\":\"{{ value_json['" + name + "'] }}\"\n{% endif %} }"
+            else:
+                component_type = "sensor"
 
-            default_component_type = "binary_sensor" if feature_type == "Event" else "sensor"
 
-            overrides = feature.get("discovery", {})
+            defaultValue = None
+            if initValue is not None:
+                if values is not None and initValue in values:
+                    defaultValue = values[initValue]
+                elif component_type == "binary_sensor":
+                    defaultValue = initValue == "1"
 
-            component_type = overrides.get("component_type", default_component_type)
+            if component_type is not "event" and defaultValue is not None:
+                value_template = "{% if '" + name + "' in value_json %}\n{{ value_json['" + name + "']|default('" + str(defaultValue) + "') }}\n{% endif %}"
 
-            extra_payload_values = overrides.get("payload_values", {})
+            # Temperature Sensor (assuming C?)
+            if refCID == "07" and refDID == "A4":
+                extra_payload_values = extra_payload_values | {"unit_of_measurement": "°C", "device_class": "temperature"}
+            elif refCID == "10" and refDID == "82":
+                extra_payload_values = extra_payload_values | {"unit_of_measurement": "s", "device_class": "duration"}
+
+#            component_type = overrides.get("component_type", default_component_type)
 
             discovery_topic = (
                 f"{HA_DISCOVERY_PREFIX}/{component_type}/hcpy/{device_ident}_{feature_id}/config"
             )
-            # print(discovery_topic, state_topic)
 
             discovery_payload = {
-                "name": decamelcase(name_parts[-1]),
+                "name": name,
                 "device": device_info,
                 "state_topic": f"{mqtt_topic}/state",
-                # "availability_topic": f"{mqtt_topic}/LWT",
-                # # I found the behaviour of `availability_topic` unsatisfactory -
-                # # since it would set all device attributes to "unavailable"
-                # # then back to their correct values on every disconnect/
-                # # reconnect. This leaves a lot of noise in the HA history, so
-                # # I felt things were better off without an `availability_topic`.
-                "value_template": "{{ value_json['" + name + "']|default('unavailable') }}",
+                "availability_mode": "all",
+                "availability": [{"topic":f"{base_topic}/LWT"}, {"topic":f"{mqtt_topic}/LWT"}],
+                "value_template": value_template,
                 "object_id": f"{device_ident}_{feature_id}",
                 "unique_id": f"{device_ident}_{feature_id}",
                 **extra_payload_values,
             }
 
-            if component_type == "binary_sensor":
-                discovery_payload.setdefault("payload_on", "On")
-                discovery_payload.setdefault("payload_off", "Off")
-
-            # print(discovery_topic)
-            # print(discovery_payload)
+            overrides = MAGIC_OVERRIDES.get(name)
+            if overrides:
+                payload_values = overrides.get("payload_values", {})
+                # Overwrite keys with override values
+                discovery_payload = discovery_payload | payload_values
 
             client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
