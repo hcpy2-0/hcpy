036e3ddee4dc3b0b074577bcf88a01cdcab51c2c
Ben Campbell <Meatballs1@users.noreply.github.com>
2025-02-21 20:38:53 +0000

    Discovery - Naming and initValue improvements (#135)
    * Better handling of initValues
    * Move discovery options to config file
    * Add network discovery
    * Make writeable binary sensors switches
    * Make writeable temperatures into numbers
    * Add select entities to HADiscovery
    * Remove debug skipping of value checks
    * Tweak values that are String integers or Named to the matching integer value
    * Model enums with only On/Off as switches
    * Use buttons where appropriate instead of switches
    * Process more entities without limiting on available
    * Handle values from description changes
    * Adjust override format
    * HA Discovery Active Program and Selected Program
    * Use overrides to ensure devices are set to buttons
    * Change dishcare options to switches. Relax checks in HCDevice to allow requests when we dont receive access changes for entities

--- HADiscovery.py
+++ HADiscovery.py
@@ -1,40 +1,23 @@
 import json
 
+import yaml
+
 from HCSocket import now
 
-HA_DISCOVERY_PREFIX = "homeassistant"
+config = {}
+try:
+    with open("discovery.yaml", "r") as yaml_config:
+        config = yaml.safe_load(yaml_config)
+except Exception:
+    print(now(), "HADiscovery - No discovery configuration file found.")
 
-# These "magic overrides" provide HA MQTT autodiscovery data.
-MAGIC_OVERRIDES = {
-    "BSH.Common.Option.ProgramProgress": {"payload_values": {"unit_of_measurement": "%"}},
-    "BSH.Common.Option.RemainingProgramTime": {
-        "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
-    },
-    "BSH.Common.Option.StartInRelative": {
-        "payload_values": {"unit_of_measurement": "s", "device_class": "duration"}
-    },
-    "BSH.Common.Status.DoorState": {"payload_values": {"icon": "mdi:door"}},
-    "Refrigeration.Common.Status.Door.Freezer": {"payload_values": {"icon": "mdi:door"}},
-    "Refrigeration.Common.Status.Door.Refrigerator": {"payload_values": {"icon": "mdi:door"}},
-}
-
-USE_FQDN = [
-    "Cooking.Hob.Status.Zone.",
-    "Dishcare.Dishwasher.Status.LearningDishwasher.Proposal.",
-    "BSH.Common.Setting.Favorite.",
-]
-
-# We don't believe these ever have state to display
-SKIP_ENTITIES = ["Dishcase.Dishwasher.Program.", "BSH.Common.Root."]
-
-# We haven't seen these display any values
-DISABLED_ENTITIES = [
-    "Refrigeration.Common.Status.",
-    "Dishcare.Dishwasher.Command.LearningDishwasher.Proposal.",
-]
-
-# Exceptions to the above
-DISABLED_EXCEPTIONS = ["Refrigeration.Common.Status.Door."]
+HA_DISCOVERY_PREFIX = config.get("HA_DISCOVERY_PREFIX", "homeassistant")
+MAGIC_OVERRIDES = config.get("MAGIC_OVERRIDES", {})
+EXPAND_NAME = config.get("EXPAND_NAME", {})
+SKIP_ENTITIES = config.get("SKIP_ENTITIES", [])
+DISABLED_ENTITIES = config.get("DISABLED_ENTITIES", [])
+DISABLED_EXCEPTIONS = config.get("DISABLED_EXCEPTIONS", [])
+ADDITIONAL_FEATURES = config.get("ADDITIONAL_FEATURES", [])
 
 
 def publish_ha_discovery(device, client, mqtt_topic):
@@ -56,20 +39,28 @@ def publish_ha_discovery(device, client, mqtt_topic):
         "manufacturer": device_description.get("brand"),
         "model": device_description.get("model"),
         "sw_version": ".".join(version_parts),
+        "suggested_area": "Kitchen",
     }
 
-    for feature in device["features"].values():
+    for key, value in device["features"].items():
+        value["uid"] = key
+
+    for feature in ADDITIONAL_FEATURES + list(device["features"].values()):
         if "name" not in feature:
             continue  # TODO we could display things based on UID?
 
         name = feature["name"]
-        extra_payload_values = {}
 
         skip = False
         # Skip Programs without state
         for skip_entity in SKIP_ENTITIES:
-            if skip_entity in name:
+            if name.startswith(skip_entity):
                 skip = True
+            if (
+                name == "BSH.Common.Root.ActiveProgram"
+                or name == "BSH.Common.Root.SelectedProgram"
+            ):
+                skip = False
 
         if skip:
             continue
@@ -77,118 +68,231 @@ def publish_ha_discovery(device, client, mqtt_topic):
         disabled = False
         # Disable Refrigeration Status that isn't populated
         for disabled_entity in DISABLED_ENTITIES:
-            if disabled_entity in name:
+            if name.startswith(disabled_entity):
                 disabled = True
                 for disabled_exception in DISABLED_EXCEPTIONS:
                     if disabled_exception in name:
                         disabled = False
 
-        if disabled:
-            extra_payload_values["enabled_by_default"] = False
-
         friendly_name = name.split(".")[-1]
 
-        # Use fully qualified name if partial name is a known duplicate
-        for fqdn in USE_FQDN:
-            if fqdn in name:
-                friendly_name = name
+        # Use expand name if partial name is a known duplicate
+        for key, value in EXPAND_NAME.items():
+            if name.startswith(key):
+                try:
+                    parts = name.split(".")
+                    friendly_name = ".".join(parts[value:])
+                except Exception:
+                    friendly_name = name
                 break
 
+        uid = feature.get("uid", None)
         feature_id = name.lower().replace(".", "_")
         refCID = feature.get("refCID", None)
         refDID = feature.get("refDID", None)
         handling = feature.get("handling", None)
         access = feature.get("access", "").lower()
-        available = feature.get("available", False)
+        # available = feature.get("available", False)
         initValue = feature.get("initValue", None)
+        value = feature.get("value", None)
         values = feature.get("values", None)
         # fmt: off
-        value_template = (
+        value_template = feature.get("template", (
             "{% if '" + name + "' in value_json %}\n"
             + "{{ value_json['" + name + "']|default }}\n"
             + "{% endif %}"
-        )
+        ))
         # fmt: off
         state_topic = f"{mqtt_topic}/state"
 
+        discovery_payload = {
+            "name": friendly_name,
+            "device": device_info,
+            "state_topic": state_topic,
+            "availability_mode": "all",
+            "availability": [{"topic": f"{base_topic}/LWT"}, {"topic": f"{mqtt_topic}/LWT"}],
+            "object_id": f"{device_ident}_{feature_id}",
+            "unique_id": f"{device_ident}_{feature_id}",
+            "value_template": value_template,
+            "enabled_by_default": not disabled
+        }
+
+        entity_category = feature.get("entity_category", None)
+        if entity_category is not None:
+            discovery_payload["entity_category"] = entity_category
+
+        overrides = MAGIC_OVERRIDES.get(name, None)
+        override_component_type = None
+        if overrides:
+            override_component_type = overrides.get("component_type", None)
+
         if (
-            handling
-            or refCID is None
-            or (available and (access == "read" or access == "readwrite"))
+                (refCID == "01" and (refDID == "00" or refDID == "01"))
+                or override_component_type == "binary_sensor"
         ):
-            if refCID == "01" and refDID == "00":
-                component_type = "binary_sensor"
-                extra_payload_values["payload_on"] = True
-                extra_payload_values["payload_off"] = False
-            elif handling is not None:
-                component_type = "event"
-                extra_payload_values["event_types"] = list(values.values())
-                extra_payload_values["platform"] = "event"
-                # fmt: off
-                value_template = (
-                    "{ {% if '" + name + "' in value_json %}\n"
-                    + '"event_type":"{{ value_json[\'' + name + "'] }}\"\n"
-                    + "{% endif %} }"
-                )
-                # fmt: on
-                state_topic = f"{mqtt_topic}/event"
-            else:
-                component_type = "sensor"
-                if refCID == "03" and refDID == "80":
-                    extra_payload_values = extra_payload_values | {
-                        "device_class": "enum",
-                        "options": list(values.values()),
-                    }
-
-            defaultValue = None
-            if initValue is not None:
-                if values is not None and initValue in values:
-                    defaultValue = values[initValue]
-                elif component_type == "binary_sensor":
-                    defaultValue = initValue == "1"
-
-            if component_type != "event" and defaultValue is not None:
-                # fmt: off
-                value_template = (
-                    "{% if '" + name + "' in value_json %}\n"
-                    + "{{ value_json['" + name + "']|default('" + str(defaultValue) + "') }}\n"
-                    + "{% endif %}"
-                )
-                # fmt: on
-
-            # Temperature Sensor (assuming C?)
-            if refCID == "07" and refDID == "A4":
-                extra_payload_values = extra_payload_values | {
-                    "unit_of_measurement": "°C",
-                    "device_class": "temperature",
-                }
-            # Duration sensor e.g. Time Remaining
-            elif refCID == "10" and refDID == "82":
-                extra_payload_values = extra_payload_values | {
-                    "unit_of_measurement": "s",
-                    "device_class": "duration",
-                }
-
-            discovery_topic = (
-                f"{HA_DISCOVERY_PREFIX}/{component_type}/hcpy/{device_ident}_{feature_id}/config"
+            component_type = "binary_sensor"
+            discovery_payload["payload_on"] = True
+            discovery_payload["payload_off"] = False
+        elif handling is not None or override_component_type == "event":
+            component_type = "event"
+            discovery_payload["event_types"] = list(values.values())
+            discovery_payload["platform"] = "event"
+            # fmt: off
+            discovery_payload["value_template"] = (
+                "{ {% if '" + name + "' in value_json %}\n"
+                + '"event_type":"{{ value_json[\'' + name + "'] }}\"\n"
+                + "{% endif %} }"
             )
+            # fmt: on
+            discovery_payload["state_topic"] = f"{mqtt_topic}/event"
+        else:
+            component_type = "sensor"
 
-            discovery_payload = {
-                "name": friendly_name,
-                "device": device_info,
-                "state_topic": state_topic,
-                "availability_mode": "all",
-                "availability": [{"topic": f"{base_topic}/LWT"}, {"topic": f"{mqtt_topic}/LWT"}],
-                "value_template": value_template,
-                "object_id": f"{device_ident}_{feature_id}",
-                "unique_id": f"{device_ident}_{feature_id}",
-                **extra_payload_values,
-            }
+        defaultValue = None
+        if initValue is not None:
+            if values is not None and initValue in values:
+                defaultValue = values[initValue]
+            elif component_type == "binary_sensor":
+                if initValue.lower() == "true" or initValue.lower() == "false":
+                    defaultValue = bool(initValue)
+                else:
+                    defaultValue = initValue == "1"
+            else:
+                defaultValue = initValue
 
-            overrides = MAGIC_OVERRIDES.get(name)
-            if overrides:
-                payload_values = overrides.get("payload_values", {})
-                # Overwrite keys with override values
-                discovery_payload = discovery_payload | payload_values
+        if component_type != "event" and defaultValue is not None:
+            # fmt: off
+            discovery_payload["value_template"] = (
+                "{% if '" + name + "' in value_json %}\n"
+                + "{{ value_json['" + name + "']|default('" + str(defaultValue) + "') }}\n"
+                + "{% else %}\n"
+                + str(defaultValue) + "\n"
+                + "{% endif %}"
+            )
+            # fmt: on
 
-            client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
+        # Temperature
+        if refCID == "07" and refDID == "A4":
+            discovery_payload["unit_of_measurement"] = "°C"
+            discovery_payload["device_class"] = "temperature"
+            discovery_payload["icon"] = "mdi:thermometer"
+        elif refCID == "03" and refDID == "80":
+            if component_type != "event":
+                discovery_payload["device_class"] = "enum"
+            discovery_payload["options"] = list(values.values())
+        # Duration sensor e.g. Time Remaining
+        elif (
+                (refCID == "10" and refDID == "82")
+                or name == 'BSH.Common.Option.ElapsedProgramTime'
+                or name == 'BSH.Common.Option.Duration'
+        ):
+            discovery_payload["unit_of_measurement"] = "s"
+            discovery_payload["device_class"] = "duration"
+
+        # Setup Controllable options
+        # Access can be read, readwrite, writeonly, none
+        if (
+                ((uid is not None) and (access == "writeonly" or access == "readwrite"))
+                or override_component_type in ["button", "switch", "select", "number"]
+        ):
+            # 01/00 is binary true/false
+            # 01/01 is binary true/false only seen for Cooking.Common.Setting.ButtonTones
+            # 15/81 is accept/reject event - maybe it needs the event ID rather than true/false?
+            if (
+                    (refCID == "01" and (refDID == "00" or refDID == "01"))
+                    or (refCID == "15" and refDID == "81")
+                    or override_component_type in ["button", "switch"]
+            ):
+                if access == "writeonly" or override_component_type == "button":
+                    component_type = "button"
+                    discovery_payload["command_topic"] = f"{mqtt_topic}/set"
+                    discovery_payload["payload_press"] = f"[{{\"uid\":{uid},\"value\":true}}]"
+                    discovery_payload.pop('value_template', None)
+                else:
+                    component_type = "switch"
+                    discovery_payload["command_topic"] = f"{mqtt_topic}/set"
+                    discovery_payload["state_on"] = True
+                    discovery_payload["state_off"] = False
+                    discovery_payload["payload_on"] = f"[{{\"uid\":{uid},\"value\":true}}]"
+                    discovery_payload["payload_off"] = f"[{{\"uid\":{uid},\"value\":false}}]"
+            # 03/80 are enums
+            elif (
+                refCID == "03"
+                and refDID == "80"
+                and len(values.values()) == 2
+                and "On" in values.values()
+                and "Off" in values.values()
+            ):
+                component_type = "switch"
+                discovery_payload["command_topic"] = f"{mqtt_topic}/set"
+                discovery_payload["state_on"] = "On"
+                discovery_payload["state_off"] = "Off"
+                discovery_payload["payload_on"] = f"[{{\"uid\":{uid},\"value\":\"On\"}}]"
+                discovery_payload["payload_off"] = f"[{{\"uid\":{uid},\"value\":\"Off\"}}]"
+                discovery_payload["device_class"] = "switch"
+            elif refCID == "03" and refDID == "80":
+                component_type = "select"
+                discovery_payload["command_topic"] = f"{mqtt_topic}/set"
+                template = f"[{{\"uid\":{uid},\"value\":\"{{{{value}}}}\"}}]"
+                discovery_payload["command_template"] = template
+            # numbers
+            elif (
+                    (refCID == "07" and refDID == "A4")
+                    or (refCID == "11" and refDID == "A0")
+                    or (refCID == "02" and refDID == "80")
+                    or (refCID == "81" and refDID == "60")
+                    or (refCID == "10" and refDID == "81")
+            ):
+                component_type = "number"
+                discovery_payload["command_topic"] = f"{mqtt_topic}/set"
+                template = f"[{{\"uid\":{uid},\"value\":{{{{value}}}}}}]"
+                discovery_payload["command_template"] = template
+
+                minimum = feature.get("min", None)
+                maximum = feature.get("max", None)
+                step = feature.get("stepSize", None)
+                if minimum is not None:
+                    discovery_payload["min"] = minimum
+                if maximum is not None:
+                    discovery_payload["max"] = maximum
+                if step is not None:
+                    discovery_payload["step"] = int(step)
+
+        if name == "BSH.Common.Root.ActiveProgram" or name == "BSH.Common.Root.SelectedProgram":
+            component_type = "select"
+            options = []
+            for k, v in device['features'].items():
+                if (
+                        "name" in v
+                        and (
+                            "Dishcare.Dishwasher.Program." in v["name"]
+                            or "Cooking.Common.Program.Hood." in v["name"]
+                            or "ConsumerProducts.CoffeeMaker.Program." in v["name"]
+                            or "ConsumerProducts.CleaningRobot.Program." in v["name"]
+                            or "LaundryCare.Dryer.Program." in v["name"]
+                            or "LaundryCare.Washer.Program." in v["name"]
+                            or "LaundryCare.WasherDryer.Program." in v["name"]
+                            or "Cooking.Oven.Program." in v["name"]
+                        )
+                ):
+                    options.append(v["name"])
+
+            if len(options) < 1:
+                continue
+
+            discovery_payload["options"] = options
+            discovery_payload["command_template"] = '[{"program":"{{value}}","options":[]}]'
+            if name == "BSH.Common.Root.ActiveProgram":
+                discovery_payload["command_topic"] = f"{mqtt_topic}/activeProgram"
+            elif name == "BSH.Common.Root.SelectedProgram":
+                discovery_payload["command_topic"] = f"{mqtt_topic}/selectedProgram"
+
+        discovery_topic = (
+            f"{HA_DISCOVERY_PREFIX}/{component_type}/hcpy/{device_ident}_{feature_id}/config"
+        )
+
+        if overrides:
+            # Overwrite keys with override values
+            discovery_payload = discovery_payload | overrides
+
+        client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
