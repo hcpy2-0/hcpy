550a0afda6eecd33ee7dafcfcd7bbfcb845c5c19
Meatballs1 <1854557+Meatballs1@users.noreply.github.com>
2026-05-04 15:44:57 +0100

    Revert socket debug level

--- hc2mqtt.py
+++ hc2mqtt.py
@@ -305,7 +305,7 @@ def client_connect(
     retry_delay = 5
     while not (shutdown and shutdown.is_set()):
         try:
-            ws = HCSocket(host, device["key"], device.get("iv", None), domain_suffix, debug)
+            ws = HCSocket(host, device["key"], device.get("iv", None), domain_suffix)
             mydevice = HCDevice(ws, device, debug)
             published_state.clear()
             discovery_published = False  # Re-publish discovery on each new connection.
